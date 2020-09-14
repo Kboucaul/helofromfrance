c2bd8a5960f02706d7fdeb50629d8fe3345b1c11
Kboucaul <kboucaul@gmail.com>
2020-09-14 18:00:39 +0200

    Le mini projet est terminé, on se prépare au deploiement

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index() :Response
+    {
+        $date = new DateTime();
+        return $this->render('home.html.twig', [
+            'date'  => $date,
+            'pays'  => 'France'
+        ]);
+    }
+}
